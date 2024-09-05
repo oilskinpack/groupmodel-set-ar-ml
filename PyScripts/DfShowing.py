ee6d591c49ee96d0afa1bee4c1e2f180f8fe2ec1
Nikita Khabarov <n.khabarov@brusnika.ru>
2024-09-05 13:50:46 +0500

    File for viewing of DFs has been added

---
+++ PyScripts/DfShowing.py
@@ -0,0 +1,79 @@
+import numpy as np
+import pandas as pd
+
+version = 1
+res = ''
+
+#region Настройки отображения
+desired_width=320
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth=desired_width)
+pd.set_option('display.max_columns',15)
+#endregion
+#region Колонки
+typePN = 'Тип'
+groupModelPN = 'Группа модели'
+heightPN = 'Неприсоединенная высота'
+lengthPN = 'Длина'
+volumePN = 'Объем'
+thicknessPN = 'Толщина'
+floorPN = 'ADSK_Этаж'
+sectionPN = 'ADSK_Номер секции'
+
+parkingPN = 'ПаркингBool'
+plasteringPN = 'ШтукатурныйBool'
+mountedPN = 'НавеснойBool'
+gmCorrect = 'ПравГМBool'
+innerWall = 'ВнутрBool'
+basementPN = 'ПодвалBool'
+insulationPN = 'УтеплениеBool'
+parapetPN = 'ПарапетBool'
+concretePN = 'БетонBool'
+lluPN = 'ЛЛУBool'
+premisePN = 'ПомещенияBool'
+
+groupModelNames = ['Монолитный пилон','Монолитная стена',
+                   'Внешняя стена. Кладка','Внутренняя стена. Кладка',
+                   'Перегородка. Кладка','Штукатурка фасадная',
+                   'Навесной фасад','Навесной фасад. Кладка',
+                   'Архитектурные элементы. Специальные элементы','Отлив',
+                   'Витраж','Перегородка ГКЛ',
+                   'Обшивка ГКЛ','Зашивка ГКЛ',
+                   'Штукатурка черновая','Утепление штукатурного фасада',
+                   'Утепление навесного фасада','Утепление цоколя штукатурного',
+                   'Утепление цоколя навесного','Утепление помещений',
+                   'Невалидируемое семейство АР',
+                   'Утепление стен подвала']
+gmErrorMap = {'BRU_ФасадШтукатурный_Штукатурка_10мм':'Штукатурка фасадная',
+              'Монолитная стена подвала':'Монолитная стена',
+              'Монолитная стена лестничного-лифтового узла':'Монолитная стена',
+              'Монолитная стена приямка':'Монолитная фундаментная плита',
+              'Элемент фасада':'Архитектурные элементы. Специальные элементы',
+              'Отделка стен':'Штукатурка черновая'}
+
+x_colums = [thicknessPN,
+                   parkingPN,
+                   plasteringPN,
+                   mountedPN,
+                   innerWall,
+                   basementPN,
+                   insulationPN,
+                   parapetPN,
+                   concretePN,
+                   lluPN,
+                    premisePN,
+                   # groupModelPN]
+                     ]
+#endregion
+
+
+
+filePath = r'D:\Khabarov\Скрипты\6.Валидация АР\DataSets\ClearedDatasets'
+fileName = fr'\dataset_ar_cleared_v{version}.txt'
+df = pd.read_csv(filePath+fileName,sep=';')
+res = df
+
+res = df[df[thicknessPN] == 120][[groupModelPN,typePN,basementPN]].value_counts()
+res = df[df[premisePN] == 1][[groupModelPN,typePN]].value_counts()
+
+print(res)
